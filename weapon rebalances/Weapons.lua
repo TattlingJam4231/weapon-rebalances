c7b9556979b66ce8d28204242ab1e7e07fa11b66
MotherfuckerUnlimited <51064348+MotherfuckerUnlimited@users.noreply.github.com>
2019-05-26 18:16:06 -0700

    Added some comments

--- weapon rebalances/Weapons.lua
+++ weapon rebalances/Weapons.lua
@@ -2,10 +2,13 @@ local old_init = WeaponTweakData.init
 function WeaponTweakData:init(tweak_data)
 old_init(self, tweak_data)
 
+	--Rattlesnake Damage Buff
 	self.msr.stats.damage = 129
 
+	--Repeater Damage Buff
 	self.winchester1874.stats.damage = 129
 	
+	--Little Friend Rifle Rework
 	self.contraband.CLIP_AMMO_MAX = 30
 	self.contraband.NR_CLIPS_MAX = 4
 	self.contraband.AMMO_MAX = self.contraband.CLIP_AMMO_MAX * self.contraband.NR_CLIPS_MAX
@@ -22,10 +25,12 @@ old_init(self, tweak_data)
 		}
 	self.contraband_m203.AMMO_MAX = 4
 	
+	--KSP 58 Buffs
 	self.par.timers.reload_not_empty = 5.62
 	self.par.timers.reload_empty = 5.62
 	self.par.stats.recoil = 10
 	
+	--Vulcan Minigun Rework
 	self.m134.AMMO_MAX = 750
 	self.m134.CLIP_AMMO_MAX = 984
 	self.m134.AMMO_PICKUP = {1.5,1.5}
