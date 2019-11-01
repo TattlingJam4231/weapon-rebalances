ca307fbd981ea575d33927af3b09c333f18fc0ad
TattlingJam4231 <41134508+TattlingJam4231@users.noreply.github.com>
2019-10-31 18:46:59 -0700

    Delete raycastweaponbase.lua

--- weapon rebalances/raycastweaponbase.lua
+++
@@ -1,6 +0,0 @@
-function DOTBulletBase:start_dot_damage(col_ray, weapon_unit, dot_data, weapon_id)
-	dot_data = dot_data or self.DOT_DATA
-	local hurt_animation = not dot_data.hurt_animation_chance or math.rand(1) < dot_data.hurt_animation_chance
-
-	managers.dot:add_doted_enemy(col_ray.unit, TimerManager:game():time(), weapon_unit, dot_data.dot_length, dot_data.dot_damage, dot_data.dot_tick_period, dot_data.scale_length, dot_data.scale_damage, dot_data.dot_decay, dot_data.dot_decay_rate, hurt_animation, self.VARIANT, weapon_id)
-end
