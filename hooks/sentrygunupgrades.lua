e52070e517f711030de8a3196e258169ea2a0ceb
TattlingJam4231 <41134508+TattlingJam4231@users.noreply.github.com>
2020-07-08 06:00:56 -0700

    Update sentrygunupgrades.lua

--- hooks/sentrygunupgrades.lua
+++ hooks/sentrygunupgrades.lua
@@ -2,6 +2,6 @@ local original_init = UpgradesTweakData.init
 function UpgradesTweakData:init(...)
 	original_init(self, ...)
 	
-	self.sentry_gun_base_ammo = 700
+	self.sentry_gun_base_ammo = 600
 	
 end
