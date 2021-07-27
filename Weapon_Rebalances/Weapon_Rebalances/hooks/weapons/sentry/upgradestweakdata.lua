d5560521fdc4332dfe61919e8870ac5918fe79fe
TattlingJam4231 <oryon_1@yahoo.com>
2021-07-26 21:54:08 -0700

    update

--- Weapon_Rebalances/Weapon_Rebalances/hooks/weapons/sentry/upgradestweakdata.lua
+++ Weapon_Rebalances/Weapon_Rebalances/hooks/weapons/sentry/upgradestweakdata.lua
@@ -1,3 +1,3 @@
-Hooks:PostHook(UpgradesTweakData, "init", "WR UpgradesTweakData init", function(self)
-	self.sentry_gun_base_ammo = 600
+Hooks:PostHook(UpgradesTweakData, "_init_pd2_values", "WR UpgradesTweakData init", function(self)
+	self.sentry_gun_base_ammo = 400
 end)
